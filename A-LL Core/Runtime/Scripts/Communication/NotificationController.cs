5118153d4a6f01c597b92738e6c830e22a6322fb
Laurent Rime <laurent@a-ll.tech>
2021-02-02 16:50:08 +0100

    - Lean added - Notification controller refactored

--- A-LL Core/Runtime/Scripts/Communication/NotificationController.cs
+++ A-LL Core/Runtime/Scripts/Communication/NotificationController.cs
@@ -30,7 +30,12 @@ public class NotificationController : MonoBehaviour
 
     private bool isCurrentlyPaused = false;
     private Firebase.FirebaseApp app; //Used by Firebase
-    private List<String> topics = new List<string>();
+
+    public delegate void OnNotificationReceived(Firebase.Messaging.MessageReceivedEventArgs pushNotificationMessage);
+    public static event OnNotificationReceived notificationReceivedDelegate;
+
+    public delegate void OnDependenciesCheckedAndFixed();
+    public static event OnDependenciesCheckedAndFixed dependeciesCheckedAndFixed;
 
     private void Start()
     {
@@ -49,23 +54,8 @@ public class NotificationController : MonoBehaviour
 
                 // Set a flag here to indicate whether Firebase is ready to use by your app.
 
-                //Subscribe to Guest Topic (Topic 0)
-                Firebase.Messaging.FirebaseMessaging.SubscribeAsync("/topics/0");
-
-                ///////////////////////////////////////////////////////////////////
-                ///         /!\ STAGING RELEASE AND LIVE TESTS /!\              ///
-                ///    Comment following lines before productoin release        ///
-                ///////////////////////////////////////////////////////////////////
-
-                //Firebase.Messaging.FirebaseMessaging.SubscribeAsync("/topics/staging");    //Comment for live release, Uncomment for staging release.
-                //Debug.LogWarning("/!\\ PUSH NOTIFICATIONS FOR STAGING RELEASE ACTIVATED /!\\ \r\nDon't forget to unsubscribe to topic staging before production release.");
-                //Firebase.Messaging.FirebaseMessaging.SubscribeAsync("/topics/local");      //Comment for live or staging release, Uncomment for local tests.
-                //Debug.LogWarning("/!\\ PUSH NOTIFICATIONS FOR LOCAL TESTS ACTIVATED /!\\ \r\nDon't forget to unsubscribe to topic local before staging or production release.");
-
-                ///////////////////////////////////////////////////////////////////
-                ///         /!\ STAGING RELEASE AND LIVE TESTS /!\              ///
-                ///    Comment preceding lines before production release        ///
-                ///////////////////////////////////////////////////////////////////
+                if (dependeciesCheckedAndFixed != null)
+                    dependeciesCheckedAndFixed();
             }
             else
             {
@@ -74,25 +64,6 @@ public class NotificationController : MonoBehaviour
                 // Firebase Unity SDK is not safe to use here.
             }
         });       
-    }
-
-    public void Subscribe()
-    {
-        for (int i = 1; i <= CacheController.authCompanyConfig.privilege_level; i++)
-        {
-            string topic = "/topics/" + i;
-            Firebase.Messaging.FirebaseMessaging.SubscribeAsync(topic);
-            topics.Add(topic);
-        }
-    }
-
-    public void Unsubscribe()
-    {
-        foreach(string topic in topics)
-        {
-            Firebase.Messaging.FirebaseMessaging.SubscribeAsync(topic);
-        }
-        topics.Clear();
     }
 
 
@@ -133,50 +104,15 @@ public class NotificationController : MonoBehaviour
 
     public void OnPushNotificationReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs pushNotificationMessage)
     {
-
-        Debug.Log("OnPushNotificationReceived");
-
         //if notification has been opened
         if (pushNotificationMessage.Message.NotificationOpened)
         {
-            GlobalController.LogMe("Notifications Data : ");
             // we then extract the data from the notification
-            if (pushNotificationMessage.Message.Data.Count > 0)
-            {
-                
-            }
+            if (pushNotificationMessage.Message.Data.Count > 0 && notificationReceivedDelegate != null)
+                notificationReceivedDelegate(pushNotificationMessage);
         }
     }
 
-    private void PrepareRequest(CacheModel.NewsItem item, string id, string releaseDate, out string oldestItemId, out string oldestItemReleaseDate)
-    {
-        oldestItemReleaseDate = item.released_at;
-        oldestItemId = item.id.ToString();
-        if (DateTime.Parse(item.released_at).CompareTo(DateTime.Parse(releaseDate)) > 0)
-        {
-            oldestItemReleaseDate = releaseDate;
-            oldestItemId = id;
-        }
-    }
-
-    private void PrepareRequest(CacheModel.EventItem item, string id, string beginDate, out string oldestItemId, out string oldestItemBeginDate)
-    {
-        oldestItemBeginDate = item.begin_date;
-        oldestItemId = item.id.ToString();
-
-        if (DateTime.Parse(item.begin_date).CompareTo(DateTime.Parse(beginDate)) < 0)
-        {
-            oldestItemBeginDate = beginDate;
-            oldestItemId = id;
-        }
-    }
-
-    private void NotificationRouting(string parentView, string routingView)
-    {
-        NavigationController.GetInstance().OnDisplayOpen(parentView);
-        NavigationController.GetInstance().OnStackOpen(routingView);
-        GlobalController.LogMe("Routing to view: " + routingView);
-
-    }
+    
 
 }
