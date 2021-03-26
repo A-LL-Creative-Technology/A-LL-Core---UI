f399e56eeaf2d9f68958a1e967923098941792dc
Laurent Rime <laurent@a-ll.tech>
2021-03-26 10:52:30 +0100

    - Empty states

---
+++ Runtime/Scripts/UI/Empty States/EmptyStateController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyStateController : MonoBehaviour
+{
+    [SerializeField] private GameObject pageToRefresh;
+    // Start is called before the first frame update
+    public void OnCallToAction()
+    {
+        //Refresh Page
+        pageToRefresh.SetActive(false);
+        pageToRefresh.SetActive(true);
+    }
+}
---
+++ Runtime/Scripts/UI/Empty States/EmptyStateVariantController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyStateVariantController : MonoBehaviour
+{
+    [HideInInspector]
+    public enum BackType
+    {
+        Page,
+        Scene,
+    }
+
+    [SerializeField] private BackType backTo;
+
+    public void OnCallToAction()
+    {
+        switch (backTo)
+        {
+            case BackType.Page :
+                NavigationController.GetInstance().OnStackClose();
+                break;
+            case BackType.Scene:
+                NavigationController.GetInstance().OnLoadNextSceneAsync("Main");
+                break;
+        }
+    }
+}
