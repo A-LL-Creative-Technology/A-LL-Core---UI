a76ac34a123004f0dc304d1a4862d0bbc9e6d277
Laurent Rime <laurent@a-ll.tech>
2021-07-19 14:09:40 +0200

    - post process script for changing the localization of the app on the App Store (cf. iOSAppStoreLocalization.cs)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
 
-## [1.0.0] - 03.05.2021
+## [1.0.1] - 19.07.2021 - [Unreleased]
+
+### Added
+
+- post process script for changing the localization of the app on the App Store (cf. iOSAppStoreLocalization.cs)
+
+
+## [1.0.0] - 03.05.2021 - 12.07.2021
 
 ### Added
 
---
+++ Editor/Scripts/Post Process Build/iOSAppStoreLocalization.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+public static class iOSAppStoreLocalization
+{
+    // set your languages here
+    static string[] localizations = new string[] { "en" }; 
+
+    [PostProcessBuild(int.MaxValue)]
+    static void OnBuildDone(BuildTarget target, string pathToBuiltProject)
+    {
+        ModifyInfoPList(pathToBuiltProject);
+    }
+
+    static void ModifyInfoPList(string pathToBuiltProject)
+    {
+        string path = pathToBuiltProject + "/info.plist";
+        var plist = new PlistDocument();
+        plist.ReadFromFile(path);
+        var root = plist.root;
+        CFBundleLocalizations(root);
+        plist.WriteToFile(path);
+    }
+
+    static void CFBundleLocalizations(PlistElementDict root)
+    {
+        var rootDic = root.values;
+        
+        var array = root.CreateArray("CFBundleLocalizations");
+        foreach (var localization in localizations) array.AddString(localization);
+    }
+}
