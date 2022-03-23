fb200698d73bddf353ee0f3e2a6757bafc5f35b3
Loïc Amantini <loic@a-ll.tech>
2022-03-23 16:58:08 +0100

    Add safe area calculation for left and right

--- Runtime/Scripts/UI/SafeAreaController.cs
+++ Runtime/Scripts/UI/SafeAreaController.cs
@@ -11,7 +11,9 @@ public class SafeAreaController : MonoBehaviour
     {
         TopBackground,
         TopContent,
+        LeftBackground,
         Body,
+        RightBackground,
         BottomBackground,
         BottomContent
     };
@@ -30,7 +32,7 @@ public class SafeAreaController : MonoBehaviour
 
         safeArea = Screen.safeArea;
 
-        //safeArea = new Rect(0, 100, 1000, 2000);
+        //safeArea = new Rect(100, 100, 1000, 2000);
 
         switch (safeAreaType)
         {
@@ -46,9 +48,21 @@ public class SafeAreaController : MonoBehaviour
 
                 break;
 
+            case SafeAreaType.LeftBackground:
+
+                AdjustSafeAreaLeftBackground();
+
+                break;
+
             case SafeAreaType.Body:
 
                 AdjustSafeAreaBody();
+
+                break;
+
+            case SafeAreaType.RightBackground:
+
+                AdjustSafeAreaRightBackground();
 
                 break;
 
@@ -71,31 +85,53 @@ public class SafeAreaController : MonoBehaviour
         // adjust header
         RectTransform topContainerRectTransform = transform.GetComponent<RectTransform>();
         topContainerRectTransform.anchorMin = new Vector2(topContainerRectTransform.anchorMin.x, topContainerRectTransform.anchorMin.y - (Screen.height - safeArea.position.y - safeArea.size.y) / Screen.height);
-        
+
         topContainerRectTransform.offsetMin = Vector2.zero;
         topContainerRectTransform.offsetMax = Vector2.zero;
     }
+
 
     private void AdjustSafeAreaTopContent()
     {
         // adjust header
         RectTransform topContainerRectTransform = transform.GetComponent<RectTransform>();
-        topContainerRectTransform.anchorMin = new Vector2(topContainerRectTransform.anchorMin.x, topContainerRectTransform.anchorMin.y - (Screen.height - safeArea.position.y - safeArea.size.y) / Screen.height);
-        topContainerRectTransform.anchorMax = new Vector2(topContainerRectTransform.anchorMax.x, (safeArea.position.y + safeArea.size.y) / Screen.height);
+
+        topContainerRectTransform.anchorMin = new Vector2(safeArea.position.x / Screen.width, topContainerRectTransform.anchorMin.y - (Screen.height - safeArea.position.y - safeArea.size.y) / Screen.height);
+        topContainerRectTransform.anchorMax = new Vector2((safeArea.position.x + safeArea.size.x) / Screen.width, (safeArea.position.y + safeArea.size.y) / Screen.height);
 
         topContainerRectTransform.offsetMin = Vector2.zero;
         topContainerRectTransform.offsetMax = Vector2.zero;
+    }
+
+    private void AdjustSafeAreaLeftBackground()
+    {
+        // adjust header
+        RectTransform leftContainerRectTransform = transform.GetComponent<RectTransform>();
+        leftContainerRectTransform.anchorMax = new Vector2(leftContainerRectTransform.anchorMin.x + (safeArea.position.x / Screen.width), leftContainerRectTransform.anchorMin.y);
+
+        leftContainerRectTransform.offsetMin = Vector2.zero;
+        leftContainerRectTransform.offsetMax = Vector2.zero;
     }
 
     private void AdjustSafeAreaBody()
     {
         // adjust body
         RectTransform bodyRectTransform = transform.GetComponent<RectTransform>();
-        bodyRectTransform.anchorMin = new Vector2(bodyRectTransform.anchorMin.x, (bodyRectTransform.anchorMin.y + (safeArea.position.y / Screen.height)));
-        bodyRectTransform.anchorMax = new Vector2(bodyRectTransform.anchorMax.x, bodyRectTransform.anchorMax.y - (Screen.height - safeArea.position.y - safeArea.size.y) / Screen.height);
+        bodyRectTransform.anchorMin = new Vector2(bodyRectTransform.anchorMin.x + (safeArea.position.x / Screen.width), (bodyRectTransform.anchorMin.y + (safeArea.position.y / Screen.height)));
+        bodyRectTransform.anchorMax = new Vector2(bodyRectTransform.anchorMax.x - (Screen.width - safeArea.position.x - safeArea.size.x) / Screen.width, bodyRectTransform.anchorMax.y - (Screen.height - safeArea.position.y - safeArea.size.y) / Screen.height);
 
         bodyRectTransform.offsetMin = Vector2.zero;
         bodyRectTransform.offsetMax = Vector2.zero;
+    }
+
+    private void AdjustSafeAreaRightBackground()
+    {
+        // adjust header
+        RectTransform rightContainerRectTransform = transform.GetComponent<RectTransform>();
+        rightContainerRectTransform.anchorMin = new Vector2(rightContainerRectTransform.anchorMin.x - (Screen.width - safeArea.position.x - safeArea.size.x) / Screen.height, rightContainerRectTransform.anchorMin.y);
+
+        rightContainerRectTransform.offsetMin = Vector2.zero;
+        rightContainerRectTransform.offsetMax = Vector2.zero;
     }
 
     private void AdjustSafeAreaBottomBackground()
@@ -112,8 +148,8 @@ public class SafeAreaController : MonoBehaviour
     {
         // adjust footer
         RectTransform bottomContainerRectTransform = transform.GetComponent<RectTransform>();
-        bottomContainerRectTransform.anchorMin = new Vector2(bottomContainerRectTransform.anchorMin.x, safeArea.position.y / Screen.height);
-        bottomContainerRectTransform.anchorMax = new Vector2(bottomContainerRectTransform.anchorMax.x, (bottomContainerRectTransform.anchorMax.y + (safeArea.position.y / Screen.height)));
+        bottomContainerRectTransform.anchorMin = new Vector2(safeArea.position.x / Screen.width, safeArea.position.y / Screen.height);
+        bottomContainerRectTransform.anchorMax = new Vector2((safeArea.position.x + safeArea.size.x) / Screen.width, (bottomContainerRectTransform.anchorMax.y + (safeArea.position.y / Screen.height)));
 
         bottomContainerRectTransform.offsetMin = Vector2.zero;
         bottomContainerRectTransform.offsetMax = Vector2.zero;
